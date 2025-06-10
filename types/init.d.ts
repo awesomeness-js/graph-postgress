d9ed94ee3c9f4ebc9f26daf10568e49b1370064b
scottforte <4059731+zmilkman@users.noreply.github.com>
2025-06-10 16:53:09 -0400

    test

--- types/init.d.ts
+++ types/init.d.ts
@@ -1,1 +1,16 @@
-export default init;
+declare const _default: (params: any) => Promise<{
+    defaultBatchSize: number;
+    keyById: boolean;
+    tableName_kv: any;
+    tableName_vertices: any;
+    tableName_edges: any;
+    host: any;
+    user: any;
+    port: any;
+    password: any;
+    database: any;
+    max: number;
+    idleTimeoutMillis: number;
+    connectionTimeoutMillis: number;
+}>;
+export default _default;
